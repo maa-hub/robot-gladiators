a9960a8f9a4346cf299f1386c9bb631e4bf4a67e
Maame Konadu Assasie-Gymah <maame@Maames-MacBook-Pro.local>
2021-05-01 21:32:52 -0400

    game changes

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,15 +3,20 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
-    // Alert players that they are starting the round
-    window.alert("Welcome to Robot Gladiators!");
+var fight = function(enemyName) {
+    while(enemyHealth > 0) {
+        
+    }
+    
+for(var i = 0; i < enemyNames.length; i++) {
+    console.log(enemyNames[i]);
+    console.log(i);
+    console.log(enemyNames[i] + " is at " + i + " index");
+  }
 
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
     if (promptFight === "fight" || promptFight === "FIGHT") {
@@ -60,4 +65,6 @@ var fight = function() {
         window.alert("You need to choose a valid option. Try again!");
       }
     }
-fight();
+    for(var i = 0; i < enemyNames.length; i++) {
+        fight(enemyNames[i]);
+      }
